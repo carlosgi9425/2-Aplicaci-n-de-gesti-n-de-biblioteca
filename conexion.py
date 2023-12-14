140c4dda872bf96a6f850a6f6237b651c1d6cf13
Carlos Giraldo <94994459+carlosgi9425@users.noreply.github.com>
2023-12-14 11:41:32 -0500

    Se incia el proyecto, pendiente revisionde versiones y descripción de código

---
+++ conexion.py
@@ -0,0 +1,16 @@
+import mysql.connector as  mysql
+
+def conectar():
+    try:
+        conexion = mysql.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="libros"
+            )
+        print("Se ha conectado a la DB correctamente")
+        return conexion
+    except mysql.Error as err:
+        print("(Ha ocurrido un error", err)
+        
+        
---
+++ libro.py
@@ -0,0 +1,75 @@
+from conexion import *
+
+def registar(titulo, autor, estado):
+    try:
+        con = conectar()
+        cursor = con.cursor()
+        sentencia_sql = """ INSERT INTO libro (titulo, autor, estado) 
+        values (%s, %s, %s) """
+        datos = (titulo, autor, estado)
+        cursor.execute(sentencia_sql, datos)
+        con.commit()
+        con.close()
+        return "registro correcto"
+    except mysql.Error as err:
+        print("Ha ocurrido un error", err)
+   
+        
+def mostrar():
+    datos = []
+    try: 
+         con = conectar()
+         cursor = con.cursor()
+         sentencia_sql = "Select * from libro"
+         cursor.execute(sentencia_sql, datos)
+         datos = cursor.fetchall()
+         con.close()
+    except mysql.Error as err:
+        print("Ha ocurrido un error", err)
+    return datos
+
+def buscar(id):
+    datos = []
+    try:
+        con = conectar()
+        cursor = con.cursor()
+        sentencia_sql = "Select * from libro where id=%s"
+        cursor.execute(sentencia_sql, (id, ))
+        datos = cursor.fetchall()
+        con.close()
+    except mysql.Error as err:
+        print("Ha ocurrido un error", err)
+    return datos
+
+
+def modificar(id, campo, nuevo_valor):
+    try:
+        sentencia_sql = ""
+        con = conectar()
+        cursor = con.cursor()
+        if campo == "1":
+            sentencia_sql = " UPDATE libro SET titulo =%s WHERE id=%s "
+        elif campo == "2":
+            sentencia_sql = " UPDATE libro SET autor =%s WHERE id=%s "
+        elif campo == "3":
+            sentencia_sql = " UPDATE libro SET estado =%s WHERE id=%s "
+        datos = (nuevo_valor, id)
+        cursor.execute(sentencia_sql, datos)
+        con.commit()
+        con.close()
+        return "Se actualizo correctamente"
+    except mysql.Error as err:
+        print("Ha ocurrido un error", err)
+        
+        
+def eliminar(id):
+    try:
+        con = conectar()
+        cursor = con.cursor()
+        sentencia_sql = "DELETE FROM libro WHERE id=%s"
+        cursor.execute(sentencia_sql,(id, ))
+        con.commit()
+        con.close()
+        return "Se ha elminado"
+    except mysql.Error as err:
+        print("Se ha producido un error", err)
